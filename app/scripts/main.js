f51933bf34c9f670490989d34fc7b9134e3b268f
Ryan Poplin <mobileupstate@gmail.com>
2013-09-18 16:15:45 -0400

    changes to app's index.html

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,24 @@
 Parse.initialize("YgfGDeGWOa7NNprcxhMJPHhZaKTYzxywReo8ayth", "8SVlwGI2DE9XYG1T7TQCKjrewGB7qF0X2qhsSBGb");
 
+var ChatClass = Parse.Object.extend("Chat");
+
+var ChatCollection = Parse.Collection.extend({
+	model: ChatClass
+});
+
+var chat = new ChatCollection();
+
+$(function(){
+
+	// function();
+
+	$('.submit-btn').click(function(){
+		var message = $('.message-field').val();
+		// function();
+	});
+
+});
+
+function addMessage () {
+	// collection add...
+}
