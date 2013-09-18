808b95a0d553b5928f51e27b731f460ea82b39e9
Ryan Poplin <mobileupstate@gmail.com>
2013-09-18 14:39:41 -0400

    Our initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,2 @@
+Parse.initialize("YgfGDeGWOa7NNprcxhMJPHhZaKTYzxywReo8ayth", "8SVlwGI2DE9XYG1T7TQCKjrewGB7qF0X2qhsSBGb");
+
---
+++ test/spec/test.js
@@ -0,0 +1,10 @@
+Parse.initialize("YgfGDeGWOa7NNprcxhMJPHhZaKTYzxywReo8ayth", "8SVlwGI2DE9XYG1T7TQCKjrewGB7qF0X2qhsSBGb");
+/*global describe, it */
+'use strict';
+(function () {
+    describe('NinjaChatr App', function () {
+        it('should confirm that jQuery is avaliable', function () {
+        	expect(jQuery() !== undefined).to.equal(true);
+        });
+    });
+})();
