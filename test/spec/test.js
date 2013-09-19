f4b453645f2759597044e93817938540fad054d3
Brandon Miller <brandonevanmiller@gmail.com>
2013-09-19 08:11:12 -0400

    Wrote a test and altered font colors and shadows

--- test/spec/test.js
+++ test/spec/test.js
@@ -6,5 +6,11 @@ Parse.initialize("YgfGDeGWOa7NNprcxhMJPHhZaKTYzxywReo8ayth", "8SVlwGI2DE9XYG1T7T
         it('should confirm that jQuery is avaliable', function () {
         	expect(jQuery() !== undefined).to.equal(true);
         });
+
+        it('should store a user input into the variable message upon a submission', function(){
+        	$('.message-field').val('This is a Message')
+        	$('.submit-btn').click()
+        	expect(message).to.equal('This is a Message')
+        })
     });
 })();
